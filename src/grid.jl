58610f52cdd5eb36bf6bff2f2dcca5eab1548ef6
mbouyges <maxime.bouyges@onera.fr>
2022-11-23 10:43:34 +0100

    formatter + AbstractArray->Array

--- src/grid.jl
+++ src/grid.jl
@@ -1,4 +1,6 @@
 """
+A distributed cartesian grid. `D` is the number of spatial dimensions.
+
 Remark : the structure could lighter if we interrogate MPI to query infos (rank, etc) instead of storing
 them here.
 """
@@ -8,7 +10,7 @@ struct DistributedGrid{D}
     coords::NTuple{D,Int} # Coordinates in the global grid (1-based)
     nelts::NTuple{D,Int} # Number of elts of the local grid in each direction, without halo
     noverlaps::NTuple{D,Int} # Number of overlap elements in each direction
-    coords2rank::AbstractArray{Int,D} # MPI coordinates (1-based) to MPI rank (identical to MPI.Cart_rank)
+    coords2rank::Array{Int,D} # MPI coordinates (1-based) to MPI rank (identical to MPI.Cart_rank)
 end
 
 @inline get_comm(grid::DistributedGrid) = grid.comm
@@ -21,23 +23,23 @@ end
 
 @inline get_rank(grid::DistributedGrid) = MPI.Comm_rank(get_comm(grid))
 @inline get_rank(coords::Vector{Int}, grid::DistributedGrid) = grid.coords2rank[coords]
-@inline get_rank(coords::NTuple{D,Int}, grid::DistributedGrid{D}) where D = grid.coords2rank[coords...]
+@inline get_rank(coords::NTuple{D,Int}, grid::DistributedGrid{D}) where {D} = grid.coords2rank[coords...]
 
 """
     neighbors(::DistributedGrid{D}) where D
 
 Iterators with neighbors stencil (in local reference, i.e `-1:1`) including the current position.
 """
-@inline neighbors(::DistributedGrid{D}) where D = Iterators.product(ntuple(d -> -1:1, D)...)
+@inline neighbors(::DistributedGrid{D}) where {D} = Iterators.product(ntuple(d -> -1:1, D)...)
 
 @inline get_nelts(grid::DistributedGrid) = grid.nelts
 @inline get_nelts(grid::DistributedGrid, d::Int) = grid.nelts[d]
 
 @inline nelts_with_halo(grid::DistributedGrid) = grid.nelts .+ 2 .* grid.noverlaps
-@inline nelts_with_halo(grid::DistributedGrid, d::Int) = grid.nelts[d] + 2*grid.noverlaps[d]
+@inline nelts_with_halo(grid::DistributedGrid, d::Int) = grid.nelts[d] + 2 * grid.noverlaps[d]
 
-@inline local_indices(grid::DistributedGrid{D}) where D = ntuple(d -> range(grid.noverlaps[d]+1, grid.nelts[d] + grid.noverlaps[d]), D)
-@inline local_indices(grid::DistributedGrid{D}, d::Int) where D = range(grid.noverlaps[d]+1, grid.nelts[d] + grid.noverlaps[d])
+@inline local_indices(grid::DistributedGrid{D}) where {D} = ntuple(d -> range(grid.noverlaps[d] + 1, grid.nelts[d] + grid.noverlaps[d]), D)
+@inline local_indices(grid::DistributedGrid{D}, d::Int) where {D} = range(grid.noverlaps[d] + 1, grid.nelts[d] + grid.noverlaps[d])
 
 """
     DistributedGrid(ndims::NTuple{D,Int}, nelts::NTuple{D,Int}, noverlaps::NTuple{D,Int}; init_MPI = true) where D
@@ -46,7 +48,7 @@ Iterators with neighbors stencil (in local reference, i.e `-1:1`) including the 
 in each topological dimension on each core (hence the total number of grid elements is `sum(ndims .* nelts)`). Finally,
 `noverlaps` is the number of elements overlaps (on both side of the topological direction) in each topological direction.
 """
-function DistributedGrid(ndims::NTuple{D,Int}, nelts::NTuple{D,Int}, noverlaps::NTuple{D,Int}; init_MPI = true) where D
+function DistributedGrid(ndims::NTuple{D,Int}, nelts::NTuple{D,Int}, noverlaps::NTuple{D,Int}; init_MPI=true) where {D}
     # Init MPI if necessary
     init_MPI && MPI.Init()
 
@@ -62,9 +64,9 @@ function DistributedGrid(ndims::NTuple{D,Int}, nelts::NTuple{D,Int}, noverlaps::
     # Build grid
     DistributedGrid(comm, ndims, nelts, noverlaps)
 end
-DistributedGrid(ndims::Int, nelts::Int, noverlaps::Int; init_MPI = true) = DistributedGrid((ndims,), (nelts,), (noverlaps,);  init_MPI)
+DistributedGrid(ndims::Int, nelts::Int, noverlaps::Int; init_MPI=true) = DistributedGrid((ndims,), (nelts,), (noverlaps,); init_MPI)
 
-function DistributedGrid(comm::MPI.Comm, ndims::NTuple{D,Int}, nelts::NTuple{D,Int}, noverlaps::NTuple{D,Int}) where D
+function DistributedGrid(comm::MPI.Comm, ndims::NTuple{D,Int}, nelts::NTuple{D,Int}, noverlaps::NTuple{D,Int}) where {D}
     @assert all(noverlaps .> 0) "Minimum overlap value is 1"
 
     # Determine map between coords and rank (MPI knows this map but I am not sure if it's fast or not)
@@ -82,7 +84,7 @@ function DistributedGrid(comm::MPI.Comm, ndims::NTuple{D,Int}, nelts::NTuple{D,I
 
     DistributedGrid{D}(comm, ndims, coords, nelts, noverlaps, coords2rank)
 end
-DistributedGrid(comm::MPI.Comm, ndims::Int, nelts::Int, noverlaps::Int; init_MPI = true) = DistributedGrid(comm, (ndims,), (nelts,), (noverlaps,);  init_MPI)
+DistributedGrid(comm::MPI.Comm, ndims::Int, nelts::Int, noverlaps::Int; init_MPI=true) = DistributedGrid(comm, (ndims,), (nelts,), (noverlaps,); init_MPI)
 
 """
     create_buffers(type, grid::DistributedGrid{D}, narrays::Int = 1) where D
@@ -91,7 +93,7 @@ Create send/recv buffers for MPI exchange of `Array`'s of type `type`.
 
 `narrays` is the number of arrays that will be exchanged.
 """
-function create_buffers(grid::DistributedGrid{D}, type, narrays::Int = 1) where D
+function create_buffers(grid::DistributedGrid{D}, type, narrays::Int=1) where {D}
     coords = get_coords(grid)
     recv_buffer = Dict{Int,Vector{type}}()
 
@@ -154,7 +156,7 @@ No buffer are required, they will be created. If you want to reuse buffers, chec
 functions.
 """
 function update_halo!(array::AbstractArray{T,D}, grid::DistributedGrid{D}) where {T,D}
-    update_halo!((array, ), grid)
+    update_halo!((array,), grid)
 end
 
 """
@@ -231,18 +233,18 @@ function _arrays2buffer!(buffer::Vector{T}, arrays::NTuple{N,AbstractArray{T,D}}
     # Gather all elements index to copy into buffer
     rh = ntuple(
         d ->
-        stencil[d] < 0 ? range(noverlaps[d] + 1; length = noverlaps[d]) :
-        stencil[d] > 0 ? range(n[d] + 1, n[d] + noverlaps[d]) :
-        local_indices(grid,d),
+            stencil[d] < 0 ? range(noverlaps[d] + 1; length=noverlaps[d]) :
+            stencil[d] > 0 ? range(n[d] + 1, n[d] + noverlaps[d]) :
+            local_indices(grid, d),
         D
     )
 
     # Copy to buffer. Depending on the stencil, there might be zero, one, several elements of each dimension
     # of each array to copy in the buffer. Array elements are interlaced this way in the buffer:
     # buffer = [array1[1,2], array2[1,2], array3[1,2], array1[7,4], array2[7,4], array3[7,4], ....]
-    for (i,ind) in enumerate(Iterators.product(rh...))
+    for (i, ind) in enumerate(Iterators.product(rh...))
         for (iarray, array) in enumerate(arrays)
-            buffer[iarray + (i-1)*N] = array[ind...]
+            buffer[iarray+(i-1)*N] = array[ind...]
         end
     end
 end
@@ -256,17 +258,17 @@ function _buffer2arrays!(buffer::Vector{T}, arrays::NTuple{N,AbstractArray{T,D}}
     # Gather all elements index to copy into array
     rh = ntuple(
         d ->
-        stencil[d] < 0 ? range(1,get_noverlap(grid,d)) :
-        stencil[d] > 0 ? range(nelts_h[d] - get_noverlap(grid,d) + 1, nelts_h[d]) :
-        local_indices(grid, d),
+            stencil[d] < 0 ? range(1, get_noverlap(grid, d)) :
+            stencil[d] > 0 ? range(nelts_h[d] - get_noverlap(grid, d) + 1, nelts_h[d]) :
+            local_indices(grid, d),
         D
     )
 
     # Copy from buffer. Array elements are interlaced this way in the buffer:
     # buffer = [array1[1,2], array2[1,2], array3[1,2], array1[7,4], array2[7,4], array3[7,4], ....]
-    for (i,ind) in enumerate(Iterators.product(rh...))
+    for (i, ind) in enumerate(Iterators.product(rh...))
         for (iarray, array) in enumerate(arrays)
-            array[ind...] = buffer[iarray + (i-1)*N]
+            array[ind...] = buffer[iarray+(i-1)*N]
         end
     end
 end
@@ -275,8 +277,8 @@ end
 Indicate if the `neighbor`, obtained with the given `stencil`, is a "true" neighbor : i.e inside the global grid
 and different from the local grid.
 """
-function _is_true_neighbor(neighbor::NTuple{D,Int}, stencil::NTuple{D,Int}, grid::DistributedGrid{D}) where D
-    return all(d -> 1 <= neighbor[d] <= get_ndims(grid,d), 1:D) && any(d -> stencil[d] != 0, 1:D)
+function _is_true_neighbor(neighbor::NTuple{D,Int}, stencil::NTuple{D,Int}, grid::DistributedGrid{D}) where {D}
+    return all(d -> 1 <= neighbor[d] <= get_ndims(grid, d), 1:D) && any(d -> stencil[d] != 0, 1:D)
 end
 
-finalize_grid(::DistributedGrid; finalize_MPI = true) = finalize_MPI && MPI.Finalize()
+finalize_grid(::DistributedGrid; finalize_MPI=true) = finalize_MPI && MPI.Finalize()
